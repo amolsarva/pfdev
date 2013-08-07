d60a2df27e1d0c4ef65cc6d53cf83b487b888ded
Matthew Rauen <rauen@college.harvard.edu>
2013-08-07 14:04:45 -0400

    updated call to latest version

--- call.js
+++ call.js
@@ -1,6 +1,6 @@
         // note that num must be obtained
-        var link=LINX_URL+"clientId="+LINX_CLIENT_ID+"&msisdn="+num+"&requestId="+uniqid('1linx_');
-        var sendURL=URL_LINX_AUTH+"link="+encodeValue(link);
+        var link = "msisdn="+num;
+        var sendURL=URL_LINX_AUTH+link;
         $.ajax({
             type:           'GET',
             url:            sendURL,
@@ -8,12 +8,7 @@
             dataType:       'JSONP', 
             jsonpCallback:  "jsonpcallback",
             success:        function(res){
-              // formats and puts the return in div id="result"
-              document.getElementById("result").innerHTML = "";
-              var result = JSON.stringify(JSON.parse(res), undefined, 2);
-              $("#result").append("<pre class="+ "\"" +"prettyprint"+ "\""+ ">" + result + "</pre>");
-              $.getScript("https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?lang=css&skin=sunburst");
-              client.log(" - reply:\n"+res);
+              // use the result here
               eval(callback.replace("%1",res));
             },
             error: function (xhr, ajaxOptions, thrownError) {
